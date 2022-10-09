26be30546fb39f9980c2271975f4d0c7a1be22a8
unknown <b0975615510@gmail.com>
2022-10-09 23:18:44 +0300

    improving test design

--- pages/shellcoders_handbook_page.py
+++ pages/shellcoders_handbook_page.py
@@ -2,13 +2,14 @@ from .base_page import BasePage
 from .locators import PromoNewYearPageLocator
 from selenium.webdriver.common.by import By
 
+
 class ShellcodersHandbookPage(BasePage):
     def add_to_basket_page(self):
         basket_button = self.browser.find_element(*PromoNewYearPageLocator.ADD_BUTTON)
         basket_button.click()
 
     def should_be_purchase(self):
-        # self.should_be_shellcoder_text()
+        self.should_be_shellcoder_text()
         self.should_be_9_99_price()
 
     def should_be_basket_button(self):
@@ -18,10 +19,10 @@ class ShellcodersHandbookPage(BasePage):
         # Сообщение о том, что товар добавлен в корзину. Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили
         act_result = self.browser.find_element(*PromoNewYearPageLocator.NAME2_BOOK).text
         exp_result = self.browser.find_element(*PromoNewYearPageLocator.NAME1_BOOK).text
-        assert exp_result in act_result, "Should be The shellcoder's handbook"
+        assert exp_result == act_result, f"The title of book {exp_result} does not match the title of book {act_result}"
 
     def should_be_9_99_price(self):
         # Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара
         act_result = self.browser.find_element(*PromoNewYearPageLocator.PRICE_TEXT_MESSAGE).text
         exp_result = self.browser.find_element(*PromoNewYearPageLocator.PRICE_TEXT).get_attribute('innerHTML')
-        assert exp_result[:-9] in act_result, f"Price {exp_result[:-9]} is not presented {act_result}"
+        assert exp_result == act_result, f"Price {exp_result} is not equal to price {act_result}"
--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,28 @@
+import pytest
 from pages.main_page import MainPage
 from pages.shellcoders_handbook_page import ShellcodersHandbookPage
 import time
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+@pytest.mark.parametrize('links', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                 marks=pytest.mark.skip),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.xfail(reason="fixing this bug right now")
+def test_guest_can_add_product_to_basket(browser, links):
+    link = links
     page = ShellcodersHandbookPage(browser, link)
     page.open()
     page.should_be_basket_button()
     page.add_to_basket_page()
     page.solve_quiz_and_get_code()
     page.should_be_purchase()
-    time.sleep(1)
-
+    time.sleep(5)
